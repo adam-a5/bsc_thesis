e20c9e5af6c70e417424157ead7f8da9ef57e301
adam-a5 <adam.a5555@proton.me>
2025-04-13 14:02:45 +0200

    Fixed the bug with convex hull. It is now working for basic cases.

--- ch_experiment/src/main.rs
+++ ch_experiment/src/main.rs
@@ -3,8 +3,10 @@ fn convex_hull(points: &mut Vec<(f64, f64)>) -> Vec<(f64, f64)> {
 	let mut upper_hull: Vec<(f64, f64)> = Vec::new();
 	let mut lower_hull: Vec<(f64, f64)> = Vec::new();
 
-	// To bits is used to allow comparsions by treating it as a u64.
-	points.sort_by_key(|x| x.0.to_bits());
+	// F64 cannot be directly compared with F64 due to 
+	// the existance of special values. 
+	// TODO: Verify whether total_cmp or partial_cmp should be used here.
+	points.sort_by(|a, b| a.0.total_cmp(&b.0));
 
 	upper_hull.push(points[0].clone());
 	upper_hull.push(points[1].clone());
@@ -16,12 +18,14 @@ fn convex_hull(points: &mut Vec<(f64, f64)>) -> Vec<(f64, f64)> {
 			if upper_hull.len() <= 2 {
 				break;
 			}
+
 			let first_last = upper_hull[upper_hull.len() - 1];
 			let second_last = upper_hull[upper_hull.len() - 2];
 			let third_last = upper_hull[upper_hull.len() - 3];
-			if makes_left_or_straight_turn(third_last, second_last, first_last) {
+			if makes_right_turn(third_last, second_last, first_last) {
 				break;
 			}
+
 			upper_hull.remove(upper_hull.len() - 2);
 		}
 	}
@@ -29,19 +33,20 @@ fn convex_hull(points: &mut Vec<(f64, f64)>) -> Vec<(f64, f64)> {
 	lower_hull.push(points[points.len() - 1].clone());
 	lower_hull.push(points[points.len() - 2].clone());
 	// Second last element to first element
-	for i in (0..lower_hull.len()-2).rev() {
+	for i in (0..points.len()-1).rev() {
 		lower_hull.push(points[i].clone());
 		loop {
 			if lower_hull.len() <= 2 {
 				break;
 			}
+
 			let first_last = lower_hull[lower_hull.len() - 1];
 			let second_last = lower_hull[lower_hull.len() - 2];
 			let third_last = lower_hull[lower_hull.len() - 3];
-			if makes_left_or_straight_turn(third_last, second_last, first_last) {
+			if makes_right_turn(third_last, second_last, first_last) {
 				break;
 			}
-			// If the last three points make a left turn or straight line, break
+
 			lower_hull.remove(lower_hull.len() - 2);
 		}
 	}
@@ -59,11 +64,14 @@ fn convex_hull(points: &mut Vec<(f64, f64)>) -> Vec<(f64, f64)> {
 	final_set
 }
 
-fn makes_left_or_straight_turn(point_1: (f64,f64), point_2: (f64,f64), point_3: (f64,f64)) -> bool {
-	// The cross product can be used to determine whether a left turn was made (and a straight turn).
-	// Cross product positive or zero => left or straight turn.
+fn makes_right_turn(point_1: (f64,f64), point_2: (f64,f64), point_3: (f64,f64)) -> bool {
+	/*
+	 * The cross product can be used to determine whether a right
+	 * turn was made.
+	 * Cross product is negative => right turn.
+	 */
 	let cross_product = (point_2.0 - point_1.0)*(point_3.1-point_1.1) - (point_2.1 - point_1.1)*(point_3.0 - point_1.0);
-	cross_product >= 0.0
+	cross_product < 0.0
 }
 
 fn main() {
@@ -73,8 +81,10 @@ fn main() {
 		(3.0,0.0),
 		(4.0,3.0),
 		(3.0,5.0),
-		(2.0,3.0),
-		(4.0,2.0)
+		(0.0,4.0),
+		(6.0,4.0),
+		(2.0,2.0),
+		(3.0,1.0)
 	];
 
 	let ch = convex_hull(&mut points);
