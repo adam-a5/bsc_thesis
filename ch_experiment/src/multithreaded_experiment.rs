a6fb216ad2e3c8333647affedb4ec2141b19e8f9
adam-a5 <adam.a5555@proton.me>
2025-04-14 12:19:54 +0200

    Code clean.

--- ch_experiment/src/multithreaded_experiment.rs
+++ ch_experiment/src/multithreaded_experiment.rs
@@ -77,30 +77,6 @@ impl<
 
 		println!("Processing experiment results ...");
 
-
-		/*let mut experiment_results: Vec<(u64, f64)> = Vec::new();
-
-		for i in 3..max_vertex_count {
-
-			let mut total_ns: u128 = 0;
-
-			for _ in 0..trial_count {
-				let mut points = self.random_point_set.generate(i);
-				let start = Instant::now();
-				let _ = self.algorithm.convex_hull(&mut points);
-				let duration = start.elapsed();
-				let duration_ns = duration.as_nanos();
-				total_ns += duration_ns;
-			}
-
-			let average_duration: f64 = total_ns as f64 / trial_count as f64;
-
-			experiment_results.push((i, average_duration));
-
-			println!("Completed {} trials of {} verticies with an average of {}", trial_count, i, average_duration);
-
-		}*/
-
 		let mut results = experiment_results.lock().unwrap();
 		results.sort_by_key(|a| a.0);
 
